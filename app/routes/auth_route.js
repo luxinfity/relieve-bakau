8c4183bb2e3aa17f8b55db465e13f08e2b44cb8b
Archie Isdiningrat <archie.isdiningrat@gmail.com>
2018-11-29 13:04:01 +0700

    add root message

--- app/routes/auth_route.js
+++ app/routes/auth_route.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 const AuthController = require('../controllers/auth_controller');
 const AuthRequest = require('../middlewares/request-validator/auth_request');
 
+router.get('/', (req, res) => res.json({ message: 'bakau running' }));
+
 router.post('/register', AuthRequest('register'), AuthController.register);
 router.post('/login', AuthRequest('login'), AuthController.login);
 router.post('/refresh', AuthRequest('refresh'), AuthController.refresh);
