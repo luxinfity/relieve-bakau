4a68e7fabfce3de8bd82cdbf99c19b53e9ac1c8b
Archie Isdiningrat <archie.isdiningrat@gmail.com>
2018-12-24 16:26:23 +0700

    update rate limit message and rename controller

--- app/utils/rate_limiter.js
+++ app/utils/rate_limiter.js
@@ -9,7 +9,9 @@ module.exports = (max = config.rate.max, retry = config.rate.retry) => {
         delayMs: 0,
         handler(req, res, next) {
             res.setHeader('Retry-After', Math.ceil(retry / 1000));
-            return next(exception(`limit reach, try again in ${Math.ceil(retry / 1000)} seconds`, 429));
+            return next(exception(`limit reach, try again in ${Math.ceil(retry / 1000)} seconds`, 429, {
+                try_after: Math.ceil(retry / 1000)
+            }));
         }
     });
 
