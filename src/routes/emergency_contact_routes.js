5d651297af493901e077005ceae759f1fa1df007
Archie Isdiningrat <archie.isdiningrat@gmail.com>
2019-02-24 16:30:44 +0700

    implement repo pattern in emergency contact controller

--- src/routes/emergency_contact_routes.js
+++ src/routes/emergency_contact_routes.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 const EmergencyContactController = require('../controllers/emergency_contact_controller');
 const EmergencyContactRequest = require('../middlewares/request-validator/emergency_contact_request');
 
-router.get('/discover', EmergencyContactController.discover);
-router.get('/discover/:id', EmergencyContactController.discoverDetail);
-router.post('/nearby', EmergencyContactRequest('nearby'), EmergencyContactController.nearby);
+router.post('/discover', EmergencyContactController.discover);
+router.post('/discover/:id', EmergencyContactController.discoverDetail);
+router.get('/nearby', EmergencyContactRequest('nearby'), EmergencyContactController.nearby);
 
 module.exports = router;
