0a9b997c994ad6c2d70286733fe690fe0427f11b
Archie Isdiningrat <archie.isdiningrat@gmail.com>
2019-03-23 12:16:11 +0700

    update dependencies and missing folders

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-require('dotenv').config();
+require('dotenv').config({ path: `./environments/${process.argv[2] ? process.argv[2] : 'local'}.env` });
 
 const http = require('http');
 const app = require('./src/app');
 
 const server = http.createServer(app);
 const port = parseInt(process.env.APP_PORT, 10) || 3000;
+
 const onError = (error) => {
     if (error.syscall !== 'listen') throw error;
 
@@ -13,16 +14,16 @@ const onError = (error) => {
         : `Port ${port}`;
 
     switch (error.code) {
-    case 'EACCES':
-        console.error(`${bind} requires elevated privileges`); // eslint-disable-line
-        process.exit(1);
-        break;
-    case 'EADDRINUSE':
-        console.error(`${bind} is already in use`); // eslint-disable-line
-        process.exit(1);
-        break;
-    default:
-        throw error;
+        case 'EACCES':
+            console.error(`${bind} requires elevated privileges`); // eslint-disable-line
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(`${bind} is already in use`); // eslint-disable-line
+            process.exit(1);
+            break;
+        default:
+            throw error;
     }
 };
 
@@ -31,7 +32,7 @@ const onListening = () => {
     const bind = typeof addr === 'string'
         ? `pipe ${addr}`
         : `port ${addr.port}`;
-    console.log(`Listening on ${bind}`); // eslint-disable-line
+    console.log(`Listening on ${bind}`);  // eslint-disable-line
 };
 
 app.set('port', port);
